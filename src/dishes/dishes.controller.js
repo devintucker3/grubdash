a4432434ac2816fb32fdb0ae432a80917f04dbfd
Devin Tucker <tuckerdevin16@gmail.com>
2021-04-15 13:59:13 -0600

    updated orders and dishes controllers/routers

--- src/dishes/dishes.controller.js
+++ src/dishes/dishes.controller.js
@@ -7,3 +7,100 @@ const dishes = require(path.resolve("src/data/dishes-data"));
 const nextId = require("../utils/nextId");
 
 // TODO: Implement the /dishes handlers needed to make the tests pass
+function list(req, res, next) {
+  res.json({ data: dishes });
+}
+
+function dishExists(req, res, next) {
+  const foundDish = dishes.find((dish) => dish.id === req.params.dishId);
+  if (!foundDish) {
+    return next({ status: 404, message: "Dish does not exist" });
+  }
+
+  res.locals.dish = foundDish;
+  return next();
+}
+
+function read(req, res, next) {
+  const { dish } = res.locals;
+  res.json({ data: dish });
+}
+
+function dishHasProps(req, res, next) {
+  const { data } = req.body;
+
+  if (!data) {
+    return next({
+      status: 400,
+      message: "Include a request body with the dish's attributes.",
+    });
+  }
+
+  if (!data.name || data.name.trim().length === 0) {
+    return next({ status: 400, message: "Dish must include a name" });
+  }
+
+  if (!data.description || data.description.trim().length === 0) {
+    return next({ status: 400, message: "Dish must include a description" });
+  }
+
+  if (!data.price) {
+    return next({ status: 400, message: "Dish must include a price" });
+  }
+
+  if (
+    typeof data.price !== "number" ||
+    data.price - Math.floor(data.price) !== 0 ||
+    data.price <= 0
+  ) {
+    return next({
+      status: 400,
+      message: "Dish must have a price that is an integer greater than 0",
+    });
+  }
+
+  if (!data.image_url || data.image_url.trim().length === 0) {
+    return next({ status: 400, message: "Dish must include a image_url" });
+  }
+
+  return next();
+}
+
+function create(req, res, next) {
+  const { name, description, image_url, price } = req.body.data;
+  const id = nextId();
+
+  res
+    .status(201)
+    .json({ data: { id, name, description, image_url, price } });
+}
+
+function matchingId( req, res, next) {
+    if (!req.body.data.id) return next();
+
+    if (req.body.data.id !== req.params.dishId) {
+        return next({status: 400, message: `Dish id does not match route id. Dish: ${req.body.data.id}, Route: ${req.params.dishId}`});
+    }
+
+
+    return next();
+}
+
+function update(req, res, next) {
+    const {name, description, image_url, price} = req.body.data;
+    const {dish} = res.locals;
+
+    dish.name = name;
+    dish.description = description;
+    dish.image_url = image_url;
+    dish.price = price;
+
+    res.json({data: dish})
+}
+
+module.exports = {
+  list,
+  read: [dishExists, read],
+  create: [dishHasProps, create],
+  update: [dishExists, dishHasProps, matchingId, update],
+};
--- src/orders/orders.controller.js
+++ src/orders/orders.controller.js
@@ -7,3 +7,121 @@ const orders = require(path.resolve("src/data/orders-data"));
 const nextId = require("../utils/nextId");
 
 // TODO: Implement the /orders handlers needed to make the tests pass
+function list(req, res, next) {
+    res.json({data: orders});
+}
+
+//Order read middleware and CRUD
+function orderExists(req, res, next) {
+    const foundOrder = orders.find(order => order.id === req.params.orderId);
+    if (!foundOrder) {
+        return next({status: 404, message: `Order ${req.params.orderId} does not exist`});
+    }
+
+    res.locals.order = foundOrder
+    return next();
+}
+
+function read(req, res, next) {
+    const {order} = res.locals;
+    res.json({data: order});
+}
+
+//Order destroy middleware and CRUD
+function pendingStatusDestroy(req, res, next) {
+    const order = res.locals.order;
+
+    if(order.status !== "pending") {
+        return next({status: 400, message: "An order cannot be deleted unless it is pending"});
+    }
+
+    next();
+}
+
+function destroy(req, res, next) {
+    const orderToDelete = orders.findIndex(order => order.id === res.locals.order.id);
+    orders.splice(orderToDelete, 1);
+
+    res.sendStatus(204);
+}
+
+//Order create middleware and CRUD
+function orderHasProps(req, res, next) {
+    const {data} = req.body;
+    const {deliverTo, mobileNumber, dishes} = data;
+
+    if (!data) {
+        return next({status: 400, message: "Please include a request body with the order's attributes"});
+    }
+
+    if (!deliverTo || deliverTo.trim().length === 0) {
+        return next({status: 400, message: "Order must include a deliverTo"});
+    }
+
+    if (!mobileNumber || mobileNumber.trim().length === 0) {
+        return next({status: 400, message: "Order must include a mobileNumber"});
+    }
+
+    if (!dishes) {
+        return next({status: 400, message: "Order must include a dish"});
+    }
+
+    if (!Array.isArray(dishes) || dishes.length === 0) {
+        return next({status: 400, message: "Order must include at least one dish"});
+    }
+
+    dishes.forEach((dish, index) => {
+        const {quantity} = dish;
+        if (!quantity || typeof quantity !== "number" || quantity - Math.floor(quantity) !== 0 || quantity < 1) {
+            return next({status: 400, message: `Dish ${index} must have a quantity that is an integer greater than 0`});
+        }
+    });
+
+    next();
+}
+
+function create(req, res, next) {
+    const {deliverTo, mobileNumber, dishes} = req.body.data;
+    const id = nextId();
+
+    res.status(201).json({data: {deliverTo, mobileNumber, dishes, id}});
+}
+
+//Order update middleware and CRUD
+function pendingStatusUpdate(req, res, next) {
+    const order = req.body.data;
+
+    if (!order.status || order.status === "invalid") {
+        return next({status: 400, message: "A pending status is required to update an order."})
+    }
+
+    next();
+}
+
+function matchingId(req, res, next) {
+    if (!req.body.data.id) return next();
+
+    if(req.body.data.id !== req.params.orderId) {
+        return next({status: 400, message: `Order id does not match route id. Order: ${req.body.data.id}, Route: ${req.params.orderId}`});
+    }
+
+    return next();
+}
+
+function update(req, res, next) {
+    const {deliverTo, mobileNumber, dishes} = req.body.data;
+    const {order} = res.locals;
+    order.deliverTo = deliverTo;
+    order.mobileNumber = mobileNumber;
+    order.dishes = dishes;
+
+    res.json({data: order});
+}
+
+module.exports = {
+    list,
+    read: [orderExists, read],
+    create: [orderHasProps, create],
+    destroy: [orderExists, pendingStatusDestroy, destroy],
+    update: [orderExists, orderHasProps, pendingStatusUpdate, matchingId, update],
+}
